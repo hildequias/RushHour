f02879a76baba12ae2bc1dba1ebaf7d8e5ab0964
Hildequias Junior <juniorwelling@yahoo.com.br>
2016-11-22 16:46:29 -0200

    final project

--- RushHour/Classes/AppDelegate.cpp
+++ RushHour/Classes/AppDelegate.cpp
@@ -3,24 +3,34 @@
 
 USING_NS_CC;
 
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
-AppDelegate::AppDelegate() {
-
+AppDelegate::AppDelegate()
+{
 }
 
 AppDelegate::~AppDelegate() 
 {
 }
 
-//if you want a different context,just modify the value of glContextAttrs
-//it will takes effect on all platforms
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    //set OpenGL context attributions,now can only set six attributions:
-    //red,green,blue,alpha,depth,stencil
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
 
     GLView::setGLContextAttrs(glContextAttrs);
+}
+
+// if you want to use the package manager to install more packages,  
+// don't modify or remove this function
+static int register_all_packages()
+{
+    return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
@@ -28,16 +38,17 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLViewImpl::create("My Game");
+        
+        glview = GLViewImpl::create("RushHour");
         director->setOpenGLView(glview);
     }
-    
+
     // turn on display FPS
     director->setDisplayStats(true);
-    
+
     // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
-    
+    director->setAnimationInterval(1.0f / 60);
+
     Size screenSize = glview->getFrameSize();
     Size designSize = Size(2048, 1536);
     
@@ -63,7 +74,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
-
+    
     // if you use SimpleAudioEngine, it must be pause
     // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
@@ -71,7 +82,7 @@ void AppDelegate::applicationDidEnterBackground() {
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
-
+    
     // if you use SimpleAudioEngine, it must resume here
     // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
--- RushHour/Classes/GameLayer.cpp
+++ RushHour/Classes/GameLayer.cpp
@@ -61,18 +61,90 @@ void GameLayer::resetGame () {
 
 void GameLayer::update(float dt) {
 
+    if (!_running) return;
+
+
+    if (_player->getPositionY() < -_player->getHeight() ||
+        _player->getPositionX() < -_player->getWidth() * 0.5f) {
+
+       _running = false;
+
+    }
+
+    _player->update(dt);
+
+    _terrain->move(_player->getVector().x);
+
+    if (_player->getState() != kPlayerDying) _terrain->checkCollision(_player);
+
+    _player->place();
+
+
+    if (_player->getNextPosition().y > _screenSize.height * 0.6f) {
+        _gameBatchNode->setPositionY( (_screenSize.height * 0.6f - _player->getNextPosition().y) * 0.8f);
+    } else {
+        _gameBatchNode->setPositionY  ( 0 );
+    }
+
+    if (_terrain->getStartTerrain() && _player->getVector().x > 0) {
+
+        _speedIncreaseTimer += dt;
+        if (_speedIncreaseTimer > _speedIncreaseInterval) {
+            _speedIncreaseTimer = 0;
+            _player->setMaxSpeed (_player->getMaxSpeed() + 4);
+        }
+    }
+    
 }
 
 bool GameLayer::onTouchBegan(Touch* touch, Event* event) {
     
-    return true;
+    if (!_running) {
+        
+        if (_player->getState() == kPlayerDying) {
+            _terrain->reset();
+            _player->reset();
+            resetGame();
+        }
+        
+        return true;
+        
+    }
+    
+    if (!_terrain->getStartTerrain()) {
+        _terrain->setStartTerrain ( true );
+        return true;
+    }
+
+    if (touch) {
+	    
+                
+        if (_player->getState() == kPlayerFalling) {
+            _player->setFloating ( _player->getFloating() ? false : true );
+        
+        } else {
+            
+            if (_player->getState() !=  kPlayerDying) _player->setJumping(true);
+        }
+        return true;
+    }
+    
+    return false;
 }
 
 void GameLayer::onTouchEnded(Touch* touch, Event* event) {
-    
+    _player->setJumping(false);    
 }
 
 void GameLayer::createGameScreen () {
     
+    _gameBatchNode = SpriteBatchNode::create("blank.png", 200);
+    this->addChild(_gameBatchNode, kMiddleground);
+    
+    _terrain = Terrain::create();
+    _gameBatchNode->addChild(_terrain, kMiddleground);
+    
+    _player = Player::create();
+    _gameBatchNode->addChild(_player, kBackground);
     
 }
